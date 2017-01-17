054d63b1554ebd98fa7c23995662d133af1d50cf
lolotrgeek <jon@simplyenable.com>
2017-01-16 21:08:10 -0600

    Added sorting tabs and device geolocation

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -6,13 +6,15 @@
   selfRegistrationLoopBack.controller('HomeCtrl', HomeCtrl);
 
   selfRegistrationLoopBack.$inject =
-    ['appSpinner', 'selfRegistrationLoopBackApi', '$q', '$rootScope', '$state', '$scope', 'ngMap'];
+    ['appSpinner', 'selfRegistrationLoopBackApi', '$q', '$rootScope', '$state', '$scope', '$interval', 'ngMap'];
 
-    function HomeCtrl($rootScope, $state, selfRegistrationLoopBackApi, $scope, NgMap) {
+    function HomeCtrl($rootScope, $state, selfRegistrationLoopBackApi, $scope, $interval, NgMap, NavigatorGeolocation, GeoCoder) {
 
     var vm = this;
 
     vm.currentUser = $rootScope.currentUser; 
+	
+	vm.map = $rootScope.map;
 
     vm.getRides = getRides; 
 
@@ -22,68 +24,132 @@
 	
 	vm.saveRoute = saveRoute;
 	
-	// Show signup page if no current user
-    if ($rootScope.currentUser == null) { 
+	vm.devicePosition = null;
+
+	vm.startPosition = null;
+	
+    // Load Signup page
+	if ($rootScope.currentUser == null) { 
       $state.go('signup'); 
     }
 	
-	// Load up the Map
-	NgMap.getMap().then(function(map) {
-		$rootScope.map = map; 
-		vm.center = map.getCenter();
-		console.log('You are at' + vm.center);
+	// Load Map
+		NgMap.getMap().then(function(map) {
+		vm.map = map; 
 	  });
 	  
-	// Save current route to Database, if session dies we can recover most recent route.
-    function saveRoute() { 
+	// Get Device Location
+		
+		var options = {
+			enableHighAccuracy: true
+		};
+					
+		NavigatorGeolocation.getCurrentPosition(function(pos) {
+				vm.devicePosition = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+				
+				console.log(JSON.stringify(vm.devicePosition));                  
+			}, 
+			function(error) {                    
+				alert('Unable to get location: ' + error.message);
+			}, options);
+		
 
-		// TODO: put a timer on this
+	
+	// View Device Location
+	vm.viewDeviceLocation = function () {
+		if (vm.devicePosition !== null) {
+			vm.map.setCenter(vm.devicePosition);
+		}
+		alert('Cannot find Device Location.');
+	};
+	
+	// Start Location Device
+	vm.startLocationDevice = function () {	
+		if (vm.devicePosition !== null) {
+			vm.viewDeviceLocation();
+			vm.currentUser.route.start_address = "Your Location";
+			vm.startPosition = vm.devicePosition;
+		}
+		alert('Cannot set Device Location.');
+	};	
+	// Start Location Input
+	function startLocationInput() {
+		vm.startPosition = vm.currentUser.route.start_address; 	
+	}
+	// Geocode Start
+	 function geocodeStart() {
+		GeoCoder.geocode({address: vm.currentUser.route.start_address}).then(function(result) {
+			vm.startPosition = result;
+			
+			console.log('Address' +JSON.stringify(vm.startPosition)); 
+		});	
+	};
+  	
+	// Save Route  
+    function saveRoute() { 
         if (vm.currentUser.route.start_address  &&
             vm.currentUser.route.start_address  !== ""  &&
             vm.currentUser.route.end_address  &&
             vm.currentUser.route.end_address  !== "" ) {
-		// save to the database
+
           selfRegistrationLoopBackApi
             .saveRoute(vm.currentUser) 
             .then(function(){
                console.log("Route saved!");
-			  
+			   startLocationInput();
+			  getRides();
             });
         }
-
     }
-	// Grabs rides from Subscriber.remoteMethod in common/models/subscriber.js 
-    function getRides() {
+	// Get Rides
+    function getRides() { 
+		
 		console.log('getRides running.'); 	  
       
-	  // check if loggedin and route is set
 	  if (vm.currentUser && vm.currentUser.route) { 
         selfRegistrationLoopBackApi
 		.getRides(vm.currentUser)
           .then(function (ridesData) {			  
-			vm.ridesData = ridesData.rides;  // populate array with the ride data
+			vm.ridesData = ridesData.rides;  // populate array 'rides'
 			
-			console.log('rides gotten'); 
+			console.log('Rides gotten!'); 
 		  });
       }
 	  
     }
-	// watch for changes on the address inputs
+	
+	// watch for changes on start address
 	$scope.$watch( 'vm.currentUser.route.start_address',
 		function(newValue, oldValue){
 			console.log('Start Changed');
-			console.log(newValue);
-			console.log(oldValue);
+			console.log('new:' + newValue);
+			console.log('old:' + oldValue);
 		}
 	);
-
+	// watch for changes on start address
 	$scope.$watch( 'vm.currentUser.route.end_address',
 		function(newValue, oldValue){
 			console.log('End Changed');
-			console.log(newValue);
-			console.log(oldValue);
+			console.log('new:' + newValue);
+			console.log('old:' + oldValue);
 		}
-	);	
+	);
+	
+	// Sort Rides 
+	$scope.sortBy = function(propertyName) {
+
+		$scope.propertyName = propertyName;
+	};
+	
+	//var stopRides; // so that we can cancel updating
+	//stopRides = $interval(getRides, 1000);
+
+	// listen on DOM destroy (removal) event, and cancel the next UI update
+	// to prevent updating time after the DOM element was removed.
+	//$scope.on(getRides(), function() {
+	//$interval.cancel(stopRides);
+	//});
+		  	
 	
 	}
  
